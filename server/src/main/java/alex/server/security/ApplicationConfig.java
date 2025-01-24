3ce437e6fa2b13109ec2190bffffe1c9bdb5f88b
Ilyass-Bougati <110248648+Ilyass-Bougati@users.noreply.github.com>
2025-01-24 01:28:03 +0100

    Create ApplicationConfig.java
    Added the CustomAuthenticationFilter to the filter chain

---
+++ server/src/main/java/alex/server/security/ApplicationConfig.java
@@ -0,0 +1,27 @@
+package alex.server.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(c -> c
+                // this will be override through filters
+                .anyRequest().authenticated()
+        );
+        http.csrf(c -> c.disable());
+        http.addFilterAt(
+                new CustomAuthenticationFilter(),
+                BasicAuthenticationFilter.class
+        );
+        return http.build();
+    }
+
+}
