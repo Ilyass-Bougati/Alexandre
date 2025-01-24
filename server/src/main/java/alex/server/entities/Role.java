bc48f5307c8440deb7eddcb33ebef9bf1be679e5
Ilyass-Bougati <110248648+Ilyass-Bougati@users.noreply.github.com>
2025-01-24 21:21:56 +0100

    Update Role.java
    made the default rolename is User

--- server/src/main/java/alex/server/entities/Role.java
+++ server/src/main/java/alex/server/entities/Role.java
@@ -1,6 +1,8 @@
 package alex.server.entities;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.security.core.GrantedAuthority;
 
 @Entity
@@ -9,6 +11,7 @@ public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @ColumnDefault("USER")
     private String roleName;
 
     public long getId() {
