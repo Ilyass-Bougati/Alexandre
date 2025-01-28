f3d4ed45ac68be6babf860284f379e6034ac462a
Ilyass-Bougati <ilyass.bougati4ys@gmail.com>
2025-01-28 19:44:30 +0100

    Added CardController

--- server/src/main/java/alex/server/card/CardController.java
+++ server/src/main/java/alex/server/card/CardController.java
@@ -6,10 +6,14 @@ import alex.server.user.User;
 import alex.server.user.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/card")
@@ -17,17 +21,26 @@ public class CardController {
 
     private final AuthService authService;
     private final UserRepository userRepository;
+    private final CardRepository cardRepository;
 
-    public CardController(AuthService authService, UserRepository userRepository) {
+    public CardController(AuthService authService, UserRepository userRepository, CardRepository cardRepository) {
         this.authService = authService;
         this.userRepository = userRepository;
+        this.cardRepository = cardRepository;
     }
 
     @GetMapping("/")
-    public ResponseEntity<List<Card>> getCards(HttpSession session) {
+    public ResponseEntity<List<CardDTO>> getCards(HttpSession session) {
         CustomUserDetails userDetails = authService.getUser(session);
         User user = userDetails.getUser();
-        return ResponseEntity.ok(user.getCards());
+        // generating card list
+        List<CardDTO> cards = new ArrayList<>();
+        assert user.getCards() != null;
+        for (Card card : user.getCards()) {
+            cards.add(new CardDTO(card));
+        }
+
+        return ResponseEntity.ok(cards);
     }
 
     @PostMapping("/")
@@ -42,6 +55,48 @@ public class CardController {
         user.getCards().add(new Card(cardDTO));
         userRepository.save(user);
         return ResponseEntity.ok().build();
+    }
+
+
+    @PutMapping("/")
+    public ResponseEntity<Void> updateCard(
+            @RequestBody @Valid CardDTO cardDTO,
+            HttpSession session
+    ) {
+        CustomUserDetails userDetails = authService.getUser(session);
+        User user = userDetails.getUser();
+
+        assert user.getCards() != null;
+        if (user.getCards().stream().anyMatch(c -> c.getId() == cardDTO.getId())) {
+            user.getCards().stream().filter(c -> c.getId() == cardDTO.getId()).findFirst().ifPresent(c -> {
+                c.setCardNumber(cardDTO.getCardNumber());
+                c.setCvv(cardDTO.getCvv());
+                c.setExpiringDate(cardDTO.getExpiringDate());
+                c.setHoldersFullName(cardDTO.getHoldersFullName());
+            });
+            userRepository.save(user);
+            return ResponseEntity.ok().build();
+        } else {
+            throw new ResponseStatusException(HttpStatusCode.valueOf(404));
+        }
+    }
+
+    @DeleteMapping("/{cardId}")
+    public ResponseEntity<Void> deleteCard(
+            HttpSession session,
+            @PathVariable Long cardId
+    ) {
+        CustomUserDetails userDetails = authService.getUser(session);
+        User user = userDetails.getUser();
+        Optional<Card> card = cardRepository.findById(cardId);
+        if (card.isPresent()) {
+            assert user.getCards() != null;
+            user.getCards().remove(card.get());
+            userRepository.save(user);
+            return ResponseEntity.ok().build();
+        } else {
+            throw new ResponseStatusException(HttpStatusCode.valueOf(404));
+        }
     }
 
 }
--- server/src/main/java/alex/server/card/CardDTO.java
+++ server/src/main/java/alex/server/card/CardDTO.java
@@ -7,9 +7,18 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CardDTO {
-
+    private long id;
+    // TODO : add some full name validation
     @NotNull private String holdersFullName;
     @NotNull private String cardNumber;
     @NotNull private String expiringDate;
     @NotNull private String cvv;
+
+    public CardDTO(Card card) {
+        setHoldersFullName(card.getHoldersFullName());
+        setCardNumber(card.getCardNumber());
+        setExpiringDate(card.getExpiringDate());
+        setCvv(card.getCvv());
+        setId(card.getId());
+    }
 }
