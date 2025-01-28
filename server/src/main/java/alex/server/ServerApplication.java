e7990e401270c80d8b8897242c4bb09e30f4dc3a
Ilyass-Bougati <ilyass.bougati4ys@gmail.com>
2025-01-28 20:36:18 +0100

    Added an admin user on the startup

--- server/src/main/java/alex/server/ServerApplication.java
+++ server/src/main/java/alex/server/ServerApplication.java
@@ -1,13 +1,46 @@
 package alex.server;
 
+import alex.server.role.Role;
+import alex.server.user.User;
+import alex.server.user.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx, UserRepository userRepository) {
+        return args -> {
+            User user = new User();
+            user.setEmail("ilyass@admin.com");
+            user.setPassword("adminpassword");
+            user.getRoles().add(
+                    new Role("USER")
+            );
+            user.getRoles().add(
+                    new Role("SELLER")
+            );
+            user.getRoles().add(
+                    new Role("ADMIN")
+            );
+            user.getRoles().add(
+                    new Role("STAFF")
+            );
+            user.setFirstName("Ilyass");
+            user.setLastName("Bougati");
+
+            userRepository.save(user);
+        };
     }
 
 }
