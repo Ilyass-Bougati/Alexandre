fe4bbd8d747421b9091daf3d243a2ee98d201e3c
Ilyass-Bougati <i.bougati12@gmail.com>
2025-06-21 15:23:33 +0100

    Added traceability

--- server/OrderService/src/main/java/com/alexandre/orderservice/controller/OrderController.java
+++ server/OrderService/src/main/java/com/alexandre/orderservice/controller/OrderController.java
@@ -1,10 +1,22 @@
 package com.alexandre.orderservice.controller;
 
+import com.alexandre.orderservice.dto.OrderDTO;
+import com.alexandre.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1")
 public class OrderController {
+    private final OrderService orderService;
 
+    @PostMapping
+    public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderDTO orderDTO) {
+        return ResponseEntity.ok(orderService.createOrder(orderDTO));
+    }
 }
--- server/UserService/src/main/java/com/alexandre/userservice/controller/UserController.java
+++ server/UserService/src/main/java/com/alexandre/userservice/controller/UserController.java
@@ -1,18 +1,22 @@
 package com.alexandre.userservice.controller;
 
+import com.alexandre.userservice.dto.OrderDTO;
 import com.alexandre.userservice.dto.UserDTO;
 import com.alexandre.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1")
 public class UserController {
     private final UserService userService;
+    private final WebClient webClient;
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUser(@PathVariable UUID id) {
@@ -22,5 +26,22 @@ public class UserController {
     @PostMapping("/")
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
         return ResponseEntity.ok(userService.createUser(userDTO));
+    }
+
+    @PostMapping("/order/{userId}/{productId}")
+    public String makeOrder(@PathVariable UUID userId, @PathVariable UUID productId) {
+        OrderDTO orderDTO = OrderDTO.builder()
+                .userId(userId)
+                .productId(productId)
+                .build();
+
+        webClient.post()
+                .uri("http://localhost:8082/api/v1")
+                .bodyValue(orderDTO)
+                .retrieve()
+                .toBodilessEntity()
+                .subscribe();
+
+        return "ok";
     }
 }
