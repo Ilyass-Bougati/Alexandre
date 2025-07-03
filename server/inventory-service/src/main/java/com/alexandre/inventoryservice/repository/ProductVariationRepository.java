28a58feb57bdbe8891f166df9dd7e47e6fadd1fa
Ilyass-Bougati <i.bougati12@gmail.com>
2025-07-03 14:13:49 +0100

    Added repositories

---
+++ server/inventory-service/src/main/java/com/alexandre/inventoryservice/repository/ProductVariationRepository.java
@@ -0,0 +1,11 @@
+package com.alexandre.inventoryservice.repository;
+
+import com.alexandre.inventoryservice.entity.ProductVariation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductVariationRepository extends JpaRepository<ProductVariation, UUID> {
+}
---
+++ server/inventory-service/src/main/java/com/alexandre/inventoryservice/repository/VendorRepository.java
@@ -0,0 +1,11 @@
+package com.alexandre.inventoryservice.repository;
+
+import com.alexandre.inventoryservice.entity.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface VendorRepository extends JpaRepository<Vendor, UUID> {
+}
