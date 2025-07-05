19f9b9d25082f892e77d3e131cd510450ff7fb14
Ilyass-Bougati <i.bougati12@gmail.com>
2025-07-05 14:55:38 +0100

    Configured the authentication

--- server/user-service/src/main/java/com/alexandre/userservice/UserServiceApplication.java
+++ server/user-service/src/main/java/com/alexandre/userservice/UserServiceApplication.java
@@ -1,8 +1,11 @@
 package com.alexandre.userservice;
 
+import com.alexandre.userservice.record.JwtConverterProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(JwtConverterProperties.class)
 @SpringBootApplication
 public class UserServiceApplication {
     public static void main(String[] args) {
