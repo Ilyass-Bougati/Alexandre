8b4d707b03ceb90b01666a7010b234bb3e937d40
Ilyass-Bougati <i.bougati12@gmail.com>
2025-07-08 11:22:07 +0100

    Fixing CI : Switching from dev to test mode

---
+++ server/switch_to_test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+mv ./user-service/src/main/resources/application.yaml ./user-service/src/main/resources/application-switched.yaml
+mv ./user-service/src/main/resources/application-test.yaml ./user-service/src/main/resources/application.yaml
