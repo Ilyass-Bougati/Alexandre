06e288472ba6bb78856e262ecf4b24394e7b10dc
Ilyass-Bougati <i.bougati12@gmail.com>
2025-07-09 10:57:17 +0100

    Discovery server is working now

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
+if [[ "$*" == *"--build-all"* ]]; then
+    echo "Building all images..."
+    docker build . -f discovery-server/Dockerfile -t alexandre/discovery-server
+fi
+
+# using minikube docker
 eval $(minikube docker-env)
-docker build . -f discovery-server/Dockerfile -t alexandre/discovery-server
+
+# Building and deploying the discovery server
 kubectl apply -f k8s/discovery-server.yaml
