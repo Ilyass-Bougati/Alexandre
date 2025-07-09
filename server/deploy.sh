e4e95aba42acef3be6b34dcd86aadf668ca7fb00
Ilyass-Bougati <i.bougati12@gmail.com>
2025-07-09 23:14:30 +0100

    Cleaning things out

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/bash
 
-# using minikube docker
-eval $(minikube docker-env)
-
 # checking for the build-all flag
 # if it's present building all the images
 if [[ "$*" == *"--build-all"* ]]; then
+
+    if [[ "$*" != *"--compose"* ]]; then
+        # using minikube docker
+        eval $(minikube docker-env)
+    fi
+
     echo "Building all images..."
     docker build . -f discovery-server/Dockerfile -t alexandre/discovery-server
+    docker build . -f config-server/Dockerfile -t alexandre/config-server
 fi
 
-docker build . -f config-server/Dockerfile -t config/discovery-server
+# checking for the compose flag
+# if it's present we're using docker compose instead of k8s
+if [[ "$*" == *"--compose"* ]]; then
+    echo "Running using docker compose..."
+    docker compose up
+else
+    echo "Running using k8s..."
 
-# Building and deploying the discovery server
-kubectl apply -f k8s/discovery-server.yaml
-kubectl apply -f k8s/config-server.yaml
+    # Building and deploying the discovery server
+    kubectl apply -f k8s/discovery-server.yaml
+    kubectl apply -f k8s/config-server.yaml
+fi
