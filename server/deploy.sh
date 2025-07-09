3527fe5617c796c535ed9936982b6f9a08860881
Ilyass-Bougati <i.bougati12@gmail.com>
2025-07-09 12:34:45 +0100

    Minor changes to the deployment script

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/bash
 
+# using minikube docker
+eval $(minikube docker-env)
+
+# checking for the build-all flag
+# if it's present building all the images
 if [[ "$*" == *"--build-all"* ]]; then
     echo "Building all images..."
     docker build . -f discovery-server/Dockerfile -t alexandre/discovery-server
 fi
-
-# using minikube docker
-eval $(minikube docker-env)
 
 # Building and deploying the discovery server
 kubectl apply -f k8s/discovery-server.yaml
