72c55f09df94e94eb9d39597ecbbe766e10e50b8
Ilyass Bougati <i.bougati12@gmail.com>
2025-08-10 23:51:30 +0100

    Fixed some issues

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let accessToken = localStorage.getItem("access_token")
-let refreshToken = localStorage.getItem("refresh_token")
+// let refreshToken = localStorage.getItem("refresh_token")
 
 export const keycloakApi = axios.create({
   baseURL: process.env.NEXT_PUBLIC_KEYCLOAK_URL,
