20c5bb8dbbb9bd18006a8c6ca89528270f54e389
Ilyass-Bougati <i.bougati12@gmail.com>
2025-08-13 04:16:16 +0100

    Fixed the cors issue, this took 3 days...

---
+++ server/api-gateway/src/main/java/com/alexandre/config/GatewayCorsConfig.java
@@ -0,0 +1,24 @@
+package com.alexandre.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class GatewayCorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://ec2-13-48-111-180.eu-north-1.compute.amazonaws.com");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
