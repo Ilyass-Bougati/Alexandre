125c4cb623b9425c653b35401fb2f742a4a4ffd3
Ilyass Bougati <i.bougati12@gmail.com>
2025-08-15 16:21:30 +0100

    Fixed problems

--- client/src/app/api/products/route.ts
+++ client/src/app/api/products/route.ts
@@ -15,7 +15,7 @@ const products: Array<ProductCardProps> = [
         name: "Samsung s22 ultra",
         price: "2,600$",
         imageUrl: "/mock/samsung-s22-ultra.png",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque vel laudantium architecto aliquam adipisci officiis eius sequi hic modi, alias facilis, placeat earum fuga consequuntur delectus voluptates tempore. Architecto, nulla?"
+        description:"The Samsung Galaxy S22 Ultra is Samsung’s flagship smartphone from the S22 series, blending the power of the Galaxy S line with the productivity features of the Note series. It features a 6.8-inch Dynamic AMOLED 2X display with QHD+ resolution and a 120Hz adaptive refresh rate for ultra-smooth visuals. Powered by the Snapdragon 8 Gen 1 (or Exynos 2200 in some regions) and up to 12GB of RAM, it delivers top-tier performance."
     },
     {
         id: "3",
